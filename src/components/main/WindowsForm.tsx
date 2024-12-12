28053dec42f8e10ba98a8c6cf8330ae3ebe982c8
Emiliano Gonzalez <emigonzalez3333@gmail.com>
2024-12-11 23:10:34 -0300

    feat: :sparkles: Add validation form

--- src/components/main/WindowsForm.tsx
+++ src/components/main/WindowsForm.tsx
@@ -3,21 +3,29 @@ import { FiSend } from "react-icons/fi";
 import { WindowsBar } from "../shared/WindowsBar";
 import { FaArrowRight } from "react-icons/fa6";
 import { FiGithub } from "react-icons/fi";
-import { useState } from "react";
 import { FiLinkedin } from "react-icons/fi";
 import { MdOutlineEmail } from "react-icons/md";
 import { social } from "../../config/about_me/info";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+  email: string;
+  message: string;
+};
 
 export const WindowsForm = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-
   const { github, linkedin, email: emailSocial } = social;
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.table({ name, email, message });
+  const {
+    register,
+    handleSubmit,
+    trigger,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
   };
 
   return (
@@ -48,46 +56,78 @@ export const WindowsForm = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
           className="text-green-400 my-2 "
-          onSubmit={handleSubmit}
+          onSubmit={handleSubmit(onSubmit)}
         >
           <div className="flex flex-col mb-4">
             <label htmlFor="name">Nombre:</label>
             <input
-              className="bg-[#2D2D2D] p-2 rounded-md focus:bg-[#2D2D2D] focus:outline-none"
+              className={`bg-[#2D2D2D] p-2 rounded-md focus:bg-[#2D2D2D] focus:outline-none ${
+                errors.name ? "border border-red-500 animate-shake" : ""
+              }`}
               type="text"
               id="name"
-              name="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
+              {...register("name", {
+                required: true,
+                onBlur: () => trigger("name"),
+              })}
             />
+            {errors.name && (
+              <p className="text-red-500 text-sm">El nombre es requerido</p>
+            )}
           </div>
           <div className="flex flex-col mb-4">
             <label htmlFor="email">Correo electrónico:</label>
             <input
-              className="bg-[#2D2D2D] p-2 rounded-md focus:bg-[#2D2D2D] focus:outline-none"
+              className={`bg-[#2D2D2D] p-2 rounded-md focus:bg-[#2D2D2D] focus:outline-none ${
+                errors.email ? "border border-red-500 animate-shake" : ""
+              }`}
               type="email"
               id="email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              {...register("email", {
+                required: true,
+                pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                onBlur: () => trigger("email"),
+              })}
             />
+            {errors.email && (
+              <p className="text-red-500 text-sm">
+                El correo electrónico es requerido y debe tener un formato
+                válido
+              </p>
+            )}
           </div>
           <div className="flex flex-col mb-4">
             <label htmlFor="message">Mensaje:</label>
             <textarea
-              className="bg-[#2D2D2D] p-2 rounded-md focus:bg-[#2D2D2D] focus:outline-none resize-none"
+              className={`bg-[#2D2D2D] p-2 rounded-md focus:bg-[#2D2D2D] focus:outline-none resize-none ${
+                errors.message ? "border border-red-500 animate-shake" : ""
+              }`}
               id="message"
-              name="message"
+              {...register("message", {
+                required: true,
+                minLength: 10,
+                maxLength: 200,
+                onBlur: () => trigger("message"),
+              })}
               rows={4}
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
             />
+            {errors.message && (
+              <p className="text-red-500 text-sm">
+                {errors.message.type === "required"
+                  ? "El mensaje es requerido"
+                  : errors.message.type === "minLength"
+                  ? "El mensaje debe tener al menos 10 caracteres"
+                  : errors.message.type === "maxLength"
+                  ? "El mensaje debe tener menos de 200 caracteres"
+                  : "El mensaje es requerido"}
+              </p>
+            )}
           </div>
           <motion.button
             initial={{ opacity: 0, y: +50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1 }}
-            className="text-white flex items-center mt-4 bg-[#22C55E] py-2 px-4 rounded-lg border border-transparent"
+            className={`text-white flex items-center mt-4  py-2 px-4 rounded-lg border border-transparent ${errors.name || errors.email || errors.message ? "bg-red-500 animate-pulse" : "bg-[#22C55E]"}`}
           >
             <FiSend className="mr-2" />
             Enviar mensaje
@@ -103,13 +143,25 @@ export const WindowsForm = () => {
           <FaArrowRight className="mr-2" />$ ls social/
         </motion.p>
         <div className="text-green-400 flex gap-6">
-          <a href={github} target="_blank" className="flex items-center mt-4 hover:text-[#22C55E] hover:underline">
+          <a
+            href={github}
+            target="_blank"
+            className="flex items-center mt-4 hover:text-[#22C55E] hover:underline"
+          >
             <FiGithub className="mr-2" /> Github{" "}
           </a>
-          <a href={linkedin} target="_blank" className="flex items-center mt-4 hover:text-[#22C55E] hover:underline">
+          <a
+            href={linkedin}
+            target="_blank"
+            className="flex items-center mt-4 hover:text-[#22C55E] hover:underline"
+          >
             <FiLinkedin className="mr-2" /> Linkedin
           </a>
-          <a href={`mailto:${emailSocial}`} target="_blank" className="flex items-center mt-4 hover:text-[#22C55E] hover:underline">
+          <a
+            href={`mailto:${emailSocial}`}
+            target="_blank"
+            className="flex items-center mt-4 hover:text-[#22C55E] hover:underline"
+          >
             <MdOutlineEmail className="mr-2" /> Email
           </a>
         </div>
