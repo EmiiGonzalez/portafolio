c1dd1a8aea24966bc6e49aa6aa3fa0238dae72ec
Emiliano Gonzalez <emigonzalez3333@gmail.com>
2024-12-12 01:02:38 -0300

    style: :bug: center footer text

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,8 +2,8 @@
 export const Footer = () => {
   return (
     <footer className="bg-[#1E1E1E] p-4 text-[#9CA3AF] w-full flex flex-col items-center justify-center gap-2 mb-0">
-      <p>Hecho con ♥️ usando React y TypeScript</p>
-      <p>©2024 Emiliano Gonzalez. Todos los derechos reservados.</p>
+      <p className="text-sm text-center">Hecho con ♥️ usando React y TypeScript</p>
+      <p className="text-sm text-center">©2024 Emiliano Gonzalez. Todos los derechos reservados.</p>
     </footer>
   )
 }
